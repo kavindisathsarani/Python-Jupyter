b3d3e0b15c802ead50be78188cf2b84e0d88cd69
kavindisathsarani <dksmanimendra0@gmail.com>
2025-07-23 16:43:30 +0530

    pdb debugger

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("hello world")
+def multiflication(a, b):
+    return a * b
+
+breakpoint()
+
+x = input("Enter first number: ")
+y = input("Enter second number: ")
+
+mul = multiflication(x,y)
+print(mul)
+# This code will not work as expected because input() returns a string, and we need to convert it to an integer or float before multiplying.
